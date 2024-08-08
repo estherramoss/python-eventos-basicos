740571559d5de27e01456e8cc3ad3699b4208b89
estherramoss <130662945+estherramoss@users.noreply.github.com>
2024-08-08 15:54:03 -0300

    .

---
+++ lista_02/atividade_04.py
@@ -0,0 +1,8 @@
+# Var
+nota = int(input("Informe a nota: "))
+
+# uso if ternario
+alerta = "Deu uma de ESTER!" if nota<5 else "APROVADO!"
+
+# Alerta
+print(alerta)
---
+++ lista_02/atividade_08.py
@@ -0,0 +1,11 @@
+# listagem de nomes de alunos 
+print("=====ALUNOS=====")
+print(" ")
+
+# Array das lojas 
+nomes = ["Carol", "Esther", "Isabela", "Lívia", "Pedro"]
+
+# Exibindo Lojas
+for i, nomes in enumerate(nomes,1):
+    print(f"{i} = {nomes}")
+    print(" ")
