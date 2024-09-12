13ae81c32036f4c77ce3bb9bbdd3dad630a2aa6d
estherramoss <estherramosbocchini@gmail.com>
2024-09-12 09:58:04 -0300

    Atualização
    Atualização

--- projetos/1-py-interface/interface.py
+++ projetos/1-py-interface/interface.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 
-# Função para transferir o texto do input para Label
+# Função para transferir texto do input para label
 def mostrar_mensagem():
     texto = caixa_texto.get() # Obter o texto da caixa texto
-    label_resultado.config(text=texto) # Atualiza o texto do Label
+    label_resultado.config(text=texto) # Atualiza o texto do label
 
 # Janela principal
 janela = tk.Tk()
@@ -14,15 +14,13 @@ janela.geometry("400x150")
 caixa_texto = tk.Entry(janela, width=60)
 caixa_texto.pack(pady=10)
 
-# Criar um botão
-botao = tk.Button(janela,text= "Mostrar Texto", command=mostrar_mensagem)
+# Criar um Botão
+botao = tk.Button(janela, text= "Mostrar Texto", command=mostrar_mensagem)
 botao.pack(pady=5)
 
 # Criar Rótulo para mostrar o resultado "Label"
-label_resultado = tk.Label(janela, text=" ")
+label_resultado = tk.Label(janela, text="")
 label_resultado.pack(pady=5)
-
-
 
 # Executar a janela principal
 janela.mainloop()
